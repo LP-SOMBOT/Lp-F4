693a02e63afd82d709a2885747e7b3816dd3b1cc
LP-SOMBOT <libanyarre5@gmail.com>
2025-12-11 22:40:10 +0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -4,15 +4,14 @@ import { getDatabase } from 'firebase/database';
 
 // PLACEHOLDER CONFIG - REPLACE WITH YOUR ACTUAL FIREBASE CONFIG
 const firebaseConfig = {
-  apiKey: "AIzaSyDummyKeyForBuildPurpose",
-  authDomain: "lp-f4-quiz.firebaseapp.com",
-  databaseURL: "https://lp-f4-quiz-default-rtdb.firebaseio.com",
-  projectId: "lp-f4-quiz",
-  storageBucket: "lp-f4-quiz.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyDKFpwQU9W4Njvtmtz6N_Jc2kZjdY_CIEc",
+  authDomain: "connectsphare-a27d6.firebaseapp.com",
+  databaseURL: "https://connectsphare-a27d6-default-rtdb.firebaseio.com",
+  projectId: "connectsphare-a27d6",
+  storageBucket: "connectsphare-a27d6.firebasestorage.app",
+  messagingSenderId: "277886142393",
+  appId: "1:277886142393:web:44fedcbec4e9cc5363d868"
 };
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
